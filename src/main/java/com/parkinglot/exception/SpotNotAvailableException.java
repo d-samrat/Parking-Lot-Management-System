54c4d178db485c59b75e8b376630a24c771ec3f0
Samrat <153609011+d-samrat@users.noreply.github.com>
2026-03-03 09:51:01 +0530

    Delete src/main directory

--- src/main/java/com/parkinglot/exception/SpotNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.parkinglot.exception;
-
-public class SpotNotAvailableException extends RuntimeException {
-    public SpotNotAvailableException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/parkinglot/util/TicketIdGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.parkinglot.util;
-
-public class TicketIdGenerator {
-    public static String generateTicketId(){
-        return "TICKET_"+System.currentTimeMillis();
-    }
-}
-
